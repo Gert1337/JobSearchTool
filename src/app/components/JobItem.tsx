4c51ce6a272bdce309dfc73930ee4037612aad9a
Gert1337 <Katharinaappel@gmail.com>
2025-04-28 20:37:49 +0200

    little styling

--- src/app/components/JobItem.tsx
+++ src/app/components/JobItem.tsx
@@ -12,14 +12,14 @@ const JobItem = ({ job, updateJob, deleteJob }: JobItemProps) => {
   console.log("Job in Job:", job);
 
   return (
-    <li>
+    <div>
       <h3>
         {title} at {company}
       </h3>
       <p>Status: {status}</p>
       <button onClick={() => updateJob(_id)}>Update Status</button>
       <button onClick={() => deleteJob(_id)}>Delete</button>
-    </li>
+    </div>
   );
 };
 
--- src/app/components/JobList.tsx
+++ src/app/components/JobList.tsx
@@ -10,20 +10,25 @@ interface JobListProps {
 
 const JobList = ({ jobs, updateJob, deleteJob }: JobListProps) => {
   console.log("Jobs in JobList:", jobs);
+  if (jobs.length === 0) {
+    return (
+      <div className="text-center text-gray-500">
+        <p>No jobs found.</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>Job Listings</h2>
-      <ul>
+    <section className="w-full max-w-2xl">
+      <h2 className="text-2xl font-semibold mb-6">Job Listings</h2>
+      <ul className="flex flex-col gap-6">
         {jobs.map((job) => (
-          <JobItem
-            key={job._id}
-            job={job}
-            updateJob={updateJob}
-            deleteJob={deleteJob}
-          />
+          <li key={job._id}>
+            <JobItem job={job} updateJob={updateJob} deleteJob={deleteJob} />
+          </li>
         ))}
       </ul>
-    </div>
+    </section>
   );
 };
 
