915250bea0197f096ea8b8d72ca3ac5bb5fdb9ac
Gert1337 <Katharinaappel@gmail.com>
2025-05-03 14:16:35 +0200

    add job page

---
+++ src/app/addjob/page.tsx
@@ -0,0 +1,12 @@
+import AddJobFormWrapper from "@/components/AddJobFormWrapper";
+import React from "react";
+
+function AddJob() {
+  return (
+    <div>
+      <AddJobFormWrapper />
+    </div>
+  );
+}
+
+export default AddJob;
