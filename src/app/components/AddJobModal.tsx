aff424ae7df982457c2518f6856c0fc8be899c98
Gert1337 <Katharinaappel@gmail.com>
2025-05-04 16:57:37 +0200

    fixed modal placing and scrollbar

--- src/app/components/AddJobModal.tsx
+++ src/app/components/AddJobModal.tsx
@@ -9,15 +9,21 @@ export default function AddJobModal({ isOpen, onClose }: Props) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-opacity-40 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-3 text-gray-500 hover:text-black text-xl"
+    <div className="fixed bg-black/75 inset-0 flex items-center justify-center z-50">
+      <div className="bg-white p-5 rounded-xl shadow-lg max-h-[90vh] relative overflow-hidden">
+        <h1 className="pl-6 font-bold text-3xl">Add a Job</h1>
+        <div
+          className="overflow-y-scroll modal max-h-[80vh] pr-2 "
+          style={{ scrollbarGutter: "stable" }}
         >
-          ×
-        </button>
-        <AddJobFormWrapper />
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-4 text-gray-500 hover:text-black text-xl"
+          >
+            ×
+          </button>
+          <AddJobFormWrapper />
+        </div>
       </div>
     </div>
   );
